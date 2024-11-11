4125033f6c83a09644d72f73fb33a7bf5649febe
Robert <rromanowicz@hotmail.com>
2024-11-11 12:37:14 +0100

    oas3 model definitions

--- src/main/java/ex/rr/swaggerparser/annotation/processor/v3/OpenApiV3Processor.java
+++ src/main/java/ex/rr/swaggerparser/annotation/processor/v3/OpenApiV3Processor.java
@@ -1,24 +1,60 @@
 package ex.rr.swaggerparser.annotation.processor.v3;
 
+import static java.util.Objects.nonNull;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Stream;
+
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.tools.Diagnostic;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.palantir.javapoet.AnnotationSpec;
+import com.palantir.javapoet.ClassName;
 import com.palantir.javapoet.FieldSpec;
+import com.palantir.javapoet.MethodSpec;
+import com.palantir.javapoet.ParameterSpec;
+import com.palantir.javapoet.ParameterizedTypeName;
 import com.palantir.javapoet.TypeName;
 import com.palantir.javapoet.TypeSpec;
 
+import ex.rr.swaggerparser.annotation.Format;
 import ex.rr.swaggerparser.annotation.SwaggerClient;
 import ex.rr.swaggerparser.annotation.processor.AbstractSwaggerProcessor;
+import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.BooleanSchema;
+import io.swagger.v3.oas.models.media.DateSchema;
+import io.swagger.v3.oas.models.media.DateTimeSchema;
+import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.MapSchema;
+import io.swagger.v3.oas.models.media.NumberSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.media.UUIDSchema;
 import io.swagger.v3.parser.core.models.ParseOptions;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import lombok.Builder;
+import lombok.Data;
 
 /**
  * OpenApiV3Processor
  */
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class OpenApiV3Processor extends AbstractSwaggerProcessor {
+
+  private Format format;
+  private String parentName;
+  private Map<String, TypeSpec> definitions = new HashMap<>();
+  private Map<String, TypeSpec> enumDefinitions = new HashMap<>();
 
   public OpenApiV3Processor(ProcessingEnvironment processingEnvironment) {
     super();
@@ -31,27 +67,141 @@ public class OpenApiV3Processor extends AbstractSwaggerProcessor {
 
     try {
       final String location = element.getAnnotation(SwaggerClient.class).location();
+      format = element.getAnnotation(SwaggerClient.class).format();
       ParseOptions parseOptions = new ParseOptions();
       parseOptions.setResolve(true); // implicit
       parseOptions.setResolveFully(true);
-      openApi = new OpenAPIV3Parser().read(location, null, parseOptions);
 
-      openApi.getComponents().getSchemas().forEach((k, v) -> {
+      SwaggerParseResult result = new OpenAPIParser().readLocation(location, null, parseOptions);
+      openApi = result.getOpenAPI();
 
-        TypeSpec helloWorld = TypeSpec.classBuilder(k)
-            .addModifiers(Modifier.PUBLIC)
-            .addField(FieldSpec.builder(TypeName.get(String.class), "test", Modifier.PUBLIC).build())
-            .build();
+      openApi.getComponents().getSchemas().entrySet().stream()
+          .forEach(entry -> generateModelDefinitions(entry.getKey(), entry.getValue()));
 
-        saveClassDefinitionToFile(element, helloWorld);
+      var defs = Stream.concat(definitions.entrySet().stream().map(it -> it.getValue()),
+          enumDefinitions.entrySet().stream().map(it -> it.getValue())).toList();
 
-        System.out.println(k);
-      });
+      switch (format) {
+        case POJO -> defs.forEach(def -> saveClassDefinitionToFile(element, def));
+        case RECORD -> {
+          TypeSpec modelDef = TypeSpec.interfaceBuilder("Model")
+              .addModifiers(Modifier.PUBLIC)
+              .addTypes(defs)
+              .build();
+          saveClassDefinitionToFile(element, modelDef);
+        }
+      }
+
     } catch (Exception e) {
       messager.printMessage(Diagnostic.Kind.ERROR, "Error fetching Swagger API Metadata.");
       throw e;
     }
 
+  }
+
+  private void generateModelDefinitions(String name, Schema schema) {
+    this.parentName = name;
+
+    var def = switch (format) {
+      case POJO -> generatePOJODefinition(name, schema);
+      case RECORD -> generateRecordDefinition(name, schema);
+    };
+
+    definitions.put(name, def);
+  }
+
+  private TypeSpec generatePOJODefinition(String name, Schema<?> schema) {
+    var pojo = TypeSpec.classBuilder(name)
+        .addModifiers(Modifier.PUBLIC)
+        .addSuperinterface(ClassName.get(Serializable.class))
+        .addAnnotation(Data.class)
+        .addAnnotation(Builder.class);
+
+    Map<String, Schema> properties = schema.getProperties();
+
+    properties.forEach((k, v) -> {
+      var field = FieldSpec.builder(resolveType(v), k.replaceAll("[^a-zA-Z0-9]", ""), Modifier.PRIVATE);
+      var fieldAnnotation = getPropertyAnnotation(k, nonNull(v.getRequired()) && v.getRequired().contains(k));
+      if (!fieldAnnotation.members().isEmpty()) {
+        field.addAnnotation(fieldAnnotation);
+      }
+      pojo.addField(field.build());
+    });
+
+    return pojo.build();
+  }
+
+  private TypeSpec generateRecordDefinition(String name, Schema schema) {
+    var recordConstructor = MethodSpec.methodBuilder(name);
+    var record = TypeSpec.recordBuilder(name)
+        .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+        .addAnnotation(Builder.class);
+
+    Map<String, Schema> properties = schema.getProperties();
+    properties.forEach((k, v) -> {
+      var field = ParameterSpec.builder(resolveType(v), k.replaceAll("[^a-zA-Z0-9]", ""));
+      var fieldAnnotation = getPropertyAnnotation(k, nonNull(v.getRequired()) && v.getRequired().contains(v));
+      if (!fieldAnnotation.members().isEmpty()) {
+        field.addAnnotation(fieldAnnotation);
+      }
+      recordConstructor.addParameter(field.build());
+    });
+    record.recordConstructor(recordConstructor.build());
+    return record.build();
+  }
+
+  private AnnotationSpec getPropertyAnnotation(String name, boolean required) {
+    var cleanName = name.replaceAll("[^a-zA-Z0-9]", "");
+    var fieldAnnotation = AnnotationSpec.builder(JsonProperty.class);
+    if (!name.equals(cleanName)) {
+      fieldAnnotation.addMember("value", "\"$L\"", name);
+    }
+    if (required) {
+      fieldAnnotation.addMember("required", "$L", required);
+    }
+    return fieldAnnotation.build();
+  }
+
+  private TypeName resolveType(Schema<?> schema) {
+    if (nonNull(schema.get$ref())) {
+      ClassName.get("", resolveReferenceClassName(schema.get$ref()));
+    }
+
+    return switch (schema) {
+      case ArraySchema p -> {
+        if (nonNull(p.getUniqueItems()) && p.getUniqueItems()) {
+          yield ParameterizedTypeName.get(ClassName.get("java.util", "Set"),
+              resolveType(p.getItems()));
+        } else {
+          yield ParameterizedTypeName.get(ClassName.get("java.util", "List"),
+              resolveType(p.getItems()));
+        }
+      }
+      case BooleanSchema p -> TypeName.get(Boolean.class);
+      case StringSchema p -> TypeName.get(String.class);
+      case IntegerSchema p -> switch (p.getFormat()) {
+        case "int64" -> TypeName.get(Long.class);
+        case "int32" -> TypeName.get(Integer.class);
+        default -> TypeName.get(Integer.class);
+      };
+      case NumberSchema p -> switch (p.getType()) {
+        case "float" -> TypeName.get(Float.class);
+        case "double" -> TypeName.get(Double.class);
+        default -> TypeName.get(Float.class);
+      };
+      case DateSchema p -> TypeName.get(LocalDate.class);
+      case DateTimeSchema p -> TypeName.get(LocalDateTime.class);
+      case UUIDSchema p -> TypeName.get(UUID.class);
+      case MapSchema p -> TypeName.get(Map.class);
+      // case ObjectSchema p ->
+      default -> TypeName.get(String.class);
+
+    };
+  }
+
+  private String resolveReferenceClassName(String ref) {
+    String[] arr = ref.split("/");
+    return arr[arr.length - 1];
   }
 
 }
